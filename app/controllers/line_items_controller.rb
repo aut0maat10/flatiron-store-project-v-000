9d12b199d001b3eff39a303e184b76d452f6f2d9
Robin Pahlman <robin.pahlman@gmail.com>
2017-12-10 03:03:57 -0500

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,18 @@
 class LineItemsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    if current_user.current_cart == nil
+      @cart = Cart.create(user_id: current_user.id)
+      @cart.add_item(params[:item_id]).save
+      current_user.current_cart = @cart
+      current_user.save
+    else
+      @cart = current_user.current_cart 
+      @line_item = @cart.add_item(params[:item_id])
+      @line_item.save
+    end 
+      redirect_to cart_path(current_user.current_cart)
+  end
 
 end
